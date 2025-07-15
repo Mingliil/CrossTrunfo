e53017f143f9f2fc4411e44075933a7095b606f8
Mingliil <miglegames.lmsnts@gmail.com>
2025-07-14 21:25:05 -0300

    a

--- Managers/Interact/interact_action.gd
+++ Managers/Interact/interact_action.gd
@@ -4,7 +4,8 @@ class_name InteractAction
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var action = preload("res://Managers/Interact/interact_label.tscn")
 func _on_body_entered(body: Node2D) -> void:
-	load(action)
+	var action_label = action.instantiate()
+	add_child(action_label)
 	print(player.position)
 	pass # Replace with function body.
 
