142dd502dce9d678adae8cee6b1b2d6c1b800345
Mingliil <miglegames.lmsnts@gmail.com>
2025-07-14 21:14:58 -0300

    update

--- Managers/Interact/interact_action.gd
+++ Managers/Interact/interact_action.gd
@@ -2,9 +2,9 @@ extends Area2D
 class_name InteractAction
 
 @onready var player = get_tree().get_first_node_in_group("player")
-
+@onready var action = preload("res://Managers/Interact/interact_label.tscn")
 func _on_body_entered(body: Node2D) -> void:
-	
+	load(action)
 	print(player.position)
 	pass # Replace with function body.
 
