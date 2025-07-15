9a3ee74f576f34da0220d149d0212787b6826e01
Mingliil <miglegames.lmsnts@gmail.com>
2025-07-14 21:32:00 -0300

    test

--- Managers/Interact/interact_action.gd
+++ Managers/Interact/interact_action.gd
@@ -6,10 +6,14 @@ class_name InteractAction
 func _on_body_entered(body: Node2D) -> void:
 	var action_label = action.instantiate()
 	add_child(action_label)
-	print(player.position)
+	#var acao = 
+	if Input.is_action_pressed("Interagir"):
+		print(player.position)
+	
 	pass # Replace with function body.
 
 func _on_body_exited(body: Node2D) -> void:
-	
+	var action_label = get_tree().get_first_node_in_group("interact")
+	remove_child(action_label)
 	print("saiu")
 	pass # Replace with function body.
