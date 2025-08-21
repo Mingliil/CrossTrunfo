aec30adddbe3b8fba1c3d502aa00f73476bb07c8
Mingliil <miglegames.lmsnts@gmail.com>
2025-08-21 14:02:12 -0300

    dgfh

--- Game/playeratack.js
+++ Game/playeratack.js
@@ -6,6 +6,10 @@ Player2Stats = {
 }
 rodada = 0;
 
+
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 function ataqueP1(){
     Player2Stats.Aura = Player2Stats.Aura - 20;
     document.getElementById("VidaP2").innerHTML = Player2Stats.Aura;
@@ -18,4 +22,8 @@ function ataqueP2(){
     document.getElementById("VidaP1").innerHTML = Player1Stats.Aura;
     rodada++;
     document.getElementById("rodada").innerHTML = rodada;
+}
+
+function criaCarta(){
+document.createElement("div class=primary");
 }
